61518032f3e34a5298ac233e99ac3849c2102e3a
Techguruhacks <125970861+Techguruhack@users.noreply.github.com>
2024-06-29 22:52:22 +0530

    final commit

---
+++ sctipt.js
@@ -0,0 +1,9 @@
+function showCard() {
+    document.getElementById('cuteCard').classList.add('show');
+}
+
+function hideCard() {
+    document.getElementById('cuteCard').classList.remove('show');
+}
+
+setTimeout(showCard, 3000); 
